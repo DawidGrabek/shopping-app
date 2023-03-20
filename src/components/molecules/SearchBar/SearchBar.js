a3949e75ccd526d8405501084b96218baf0810b1
Dawid Grabek <grabekd37@gmail.com>
2023-03-20 22:49:40 +0100

    Add and configure searching among fragrances

--- src/components/molecules/SearchBar/SearchBar.js
+++ src/components/molecules/SearchBar/SearchBar.js
@@ -1,63 +1,30 @@
 import { Input } from 'components/atoms/Input/Input.styles'
 import React, { useState } from 'react'
 import { ReactComponent as SearchIcon } from 'assets/icons/search-icon.svg'
-import { SearchResult, SearchResultsItem, SearchWrapper } from './SearchBar.styles'
+import { SearchResult, SearchResultsItem, SearchWrapper, Wrapper } from './SearchBar.styles'
 import { useCombobox } from 'downshift'
-import styled from 'styled-components'
+import data from 'data'
 
-export const Wrapper = styled.div`
-  width: 100vw;
-  background-color: ${({ theme }) => theme.colors.darkGrey};
-`
-
-export const SearchBarWrapper = styled.div`
-  width: 100%;
-  max-width: 500px;
-`
-
-const books = [
-  { author: 'Harper Lee', title: 'To Kill a Mockingbird' },
-  { author: 'Lev Tolstoy', title: 'War and Peace' },
-  { author: 'Fyodor Dostoyevsy', title: 'The Idiot' },
-  { author: 'Oscar Wilde', title: 'A Picture of Dorian Gray' },
-  { author: 'George Orwell', title: '1984' },
-  { author: 'Jane Austen', title: 'Pride and Prejudice' },
-  { author: 'Marcus Aurelius', title: 'Meditations' },
-  { author: 'Fyodor Dostoevsky', title: 'The Brothers Karamazov' },
-  { author: 'Lev Tolstoy', title: 'Anna Karenina' },
-  { author: 'Fyodor Dostoevsky', title: 'Crime and Punishment' },
-]
-function getBooksFilter(inputValue) {
+const getFragrancesFilter = (inputValue) => {
   const lowerCasedInputValue = inputValue.toLowerCase()
 
-  return function booksFilter(book) {
-    return (
-      !inputValue ||
-      book.title.toLowerCase().includes(lowerCasedInputValue) ||
-      book.author.toLowerCase().includes(lowerCasedInputValue)
-    )
+  return function fragrancesFilter(fragrance) {
+    return !inputValue || fragrance.name.toLowerCase().includes(lowerCasedInputValue)
   }
 }
 
 const SearchBar = () => {
-  const [items, setItems] = React.useState(books)
-  const {
-    isOpen,
-    getToggleButtonProps,
-    getLabelProps,
-    getMenuProps,
-    getInputProps,
-    highlightedIndex,
-    getItemProps,
-  } = useCombobox({
-    onInputValueChange({ inputValue }) {
-      setItems(books.filter(getBooksFilter(inputValue)))
-    },
-    items,
-    itemToString(item) {
-      return item ? item.title : ''
-    },
-  })
+  const [items, setItems] = useState(data)
+  const { isOpen, getLabelProps, getMenuProps, getInputProps, highlightedIndex, getItemProps } =
+    useCombobox({
+      onInputValueChange({ inputValue }) {
+        setItems(data.filter(getFragrancesFilter(inputValue)))
+      },
+      items,
+      itemToString(item) {
+        return item ? item.name : ''
+      },
+    })
 
   return (
     <Wrapper>
@@ -65,16 +32,17 @@ const SearchBar = () => {
         <SearchIcon as="label" {...getLabelProps()} />
         <Input {...getInputProps()} type="text" name="Search" id="Search" placeholder="Search" />
         <SearchResult isVisible={isOpen} {...getMenuProps()}>
-          {isOpen &&
-            items.map((item, index) => (
+          {(isOpen &&
+            items.length > 1 &&
+            items.map(({ name }, index) => (
               <SearchResultsItem
                 isHighlighted={highlightedIndex === index}
                 {...getItemProps({ item: 'Mordo', index })}
-                key={item.title}
+                key={name}
               >
-                {item.title}
+                {name}
               </SearchResultsItem>
-            ))}
+            ))) || <SearchResultsItem>There are nothing with this name</SearchResultsItem>}
         </SearchResult>
       </SearchWrapper>
     </Wrapper>
