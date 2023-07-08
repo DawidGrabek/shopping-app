570b230b5c997f3452a0a11fe7f82f791e33d54e
Dawid Grabek <grabekd37@gmail.com>
2023-07-09 00:20:22 +0200

    Add Login and Profile tests

---
+++ client/src/components/pages/Login/Login.test.tsx
@@ -0,0 +1,69 @@
+import userEvent from '@testing-library/user-event'
+import { User } from 'assets/types'
+import * as hooks from 'hooks/useApi'
+import { render, screen, waitFor } from 'test-utils'
+import { vi } from 'vitest'
+
+import Login from './Login'
+
+const getMockAuth = (user: User | null) => ({
+  user,
+  signIn: vi.fn(),
+  signOut: vi.fn(),
+  signUp: vi.fn(),
+  error: '',
+  addOrder: vi.fn(),
+})
+
+describe('<Login />', () => {
+  it('Renders the login form', () => {
+    render(<Login />)
+
+    // Assert that the login form elements are rendered
+    expect(screen.getByLabelText('E-mail')).toBeInTheDocument()
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Log in' })).toBeInTheDocument()
+    expect(screen.getByText("You don't have an account?")).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'Sign up' })).toBeInTheDocument()
+  })
+
+  it('Calls the signIn function with the form data on form submission', async () => {
+    // null means that the user is not authenticated
+    const mockAuth = getMockAuth(null)
+    vi.spyOn(hooks, 'useAuth').mockReturnValue(mockAuth)
+    render(<Login />)
+
+    // Enter form data
+    userEvent.type(screen.getByLabelText('E-mail'), 'test@test.com')
+    userEvent.type(screen.getByLabelText('Password'), 'password123')
+
+    // Submit the form
+    userEvent.click(screen.getByRole('button', { name: 'Log in' }))
+
+    // Assert that the signIn function is called with the form data
+    await waitFor(() => expect(mockAuth.signIn).toHaveBeenCalledTimes(1))
+    expect(mockAuth.signIn).toHaveBeenCalledWith({
+      email: 'test@test.com',
+      password: 'password123',
+    })
+  })
+
+  it('Displays the error message when there is an authentication error', async () => {
+    // Simulate an authentication error by setting the error property in mockAuth
+    const mockAuth = getMockAuth(null)
+    mockAuth.error = 'Authentication failed'
+
+    vi.spyOn(hooks, 'useAuth').mockReturnValue(mockAuth)
+    render(<Login />)
+
+    // Enter form data
+    userEvent.type(screen.getByLabelText('E-mail'), 'test@test.com')
+    userEvent.type(screen.getByLabelText('Password'), 'password123')
+
+    // Submit the form
+    userEvent.click(screen.getByRole('button', { name: 'Log in' }))
+
+    // Assert that the error message is displayed
+    await waitFor(() => expect(screen.getByText('Authentication failed')).toBeInTheDocument())
+  })
+})
---
+++ client/src/components/pages/Profile/Profile.test.tsx
@@ -0,0 +1,78 @@
+import userEvent from '@testing-library/user-event'
+import { User } from 'assets/types'
+import * as hooks from 'hooks/useApi'
+import { render, screen } from 'test-utils'
+import { vi } from 'vitest'
+
+import Profile from './Profile'
+
+const getMockAuth = (user: User | null) => ({
+  user,
+  signIn: vi.fn(),
+  signOut: vi.fn(),
+  signUp: vi.fn(),
+  error: null,
+  addOrder: vi.fn(),
+})
+
+const mockUser = {
+  _id: '1',
+  email: 'test@test.com',
+  firstName: 'TestName',
+  lastName: 'TestSurname',
+  orders: [
+    { _id: '1', fragranceName: 'Fragrance 1', price: 150, amount: 1, capacity: 100 },
+    {
+      _id: '2',
+      fragranceName: 'Fragrance 2',
+      price: 75,
+      amount: 1,
+      capacity: 100,
+    },
+  ],
+}
+
+describe('<Profile />', () => {
+  it('Renders the user information and orders', () => {
+    vi.spyOn(hooks, 'useAuth').mockImplementation(() => getMockAuth(mockUser))
+    render(<Profile />)
+
+    // Assert that user information is displayed
+    expect(screen.getByText(`First name:`)).toBeInTheDocument()
+    expect(screen.getByText(mockUser.firstName)).toBeInTheDocument()
+
+    expect(screen.getByText(`Last name:`)).toBeInTheDocument()
+    expect(screen.getByText(mockUser.lastName)).toBeInTheDocument()
+
+    expect(screen.getByText(`Email:`)).toBeInTheDocument()
+    expect(screen.getByText(mockUser.email)).toBeInTheDocument()
+
+    // Assert that order information is displayed
+    expect(
+      screen.getByText(
+        `${mockUser.orders[0].amount} x ${mockUser.orders[0].fragranceName} - ${
+          mockUser.orders[0].price * mockUser.orders[0].amount
+        }zł`
+      )
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        `${mockUser.orders[1].amount} x ${mockUser.orders[1].fragranceName} - ${
+          mockUser.orders[1].price * mockUser.orders[1].amount
+        }zł`
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('Calls the signOut function when the log out button is clicked', () => {
+    const mockAuth = getMockAuth(mockUser)
+    vi.spyOn(hooks, 'useAuth').mockReturnValue(mockAuth)
+
+    render(<Profile />)
+
+    const logoutButton = screen.getByText('Log out')
+    userEvent.click(logoutButton)
+
+    expect(mockAuth.signOut).toHaveBeenCalledTimes(1)
+  })
+})
